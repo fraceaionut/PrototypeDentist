8ed1ac156e4364ec8604df8c29ea295f7e5790b3
Fracea Ionut <84499798+fraceaionut@users.noreply.github.com>
2026-04-02 21:51:18 +0300

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-xSrhU3P6R0U0GN9T-hEzsZ3CbGUZmLc",
+  authDomain: "prototypedentist.firebaseapp.com",
+  projectId: "prototypedentist",
+  storageBucket: "prototypedentist.firebasestorage.app",
+  messagingSenderId: "881077220842",
+  appId: "1:881077220842:web:2d162d491f9324799c6e93",
+  measurementId: "G-329N27BD25"
+};
