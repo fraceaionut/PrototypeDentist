4a0ce36c4375d5f21d2aede3da74481e1f9e78a6
Fracea Ionut <84499798+fraceaionut@users.noreply.github.com>
2026-04-04 10:38:45 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -125,7 +125,7 @@ export function bindAuth({
 
     
     if (hero) hero.style.display = "grid";
-    if(bookingActions) bookingActions.style.dysplay = "grid";
+    if(bookingActions) bookingActions.style.display = "grid";
 
     renderAppointments();
     renderTreatmentPlan();
